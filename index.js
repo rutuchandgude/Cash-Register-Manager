9f2dc3da635b1650424dd53012e814ec005dd93b
Rutuja Chandgude <74643325+rutuchandgude@users.noreply.github.com>
2021-09-22 13:46:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-const billAmt = document.querySelector("#bill-amount");
-const csshGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
+const billAmount = document.querySelector("#total_bill");
+const cashGiven = document.querySelector("#cash_given");
+const label_cash_given = document.querySelector('#label_cash_given')
+const change_table =document.querySelector('.change-table')
+const submit_button =document.getElementById('submit_button')
 const message = document.querySelector("#error-message");
-const noOfnotes = document.querySelectorAll(".no-of-notes");
-const availableNotes = [2000, 500, 100, 20, 10, 5, 2, 1]; 
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const noOfNotes_td = document.querySelectorAll(".no-of-notes_td");
 
-checkButton.addEventListener("click", function validateBillAmtAndCashAmount(){
-   hideMessage();
-        if(billAmt.value > 0) {
-            if(csshGiven.value >= billAmt.value){
-                    const amountToBeReturned = csshGiven.value - billAmt.value;
-                    calculateChange(amountToBeReturned);
-                }
-            else{
-                showMessage("Do You Wanna WASH plates!!!");
-            }
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+function opencashGiven() {
+    label_cash_given.style.display = 'block'
+    cashGiven.style.display = 'block'
+    
+}
+submit_button.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    
+    if (billAmount.value > 0) {
+        
+        if (Number(cashGiven.value) > Number(billAmount.value)) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            change_table.style.display = 'block'
+            calculateChange(amountToBeReturned);
+
         } else {
-            showMessage("Invalid Bill-Amount");
+            showMessage(
+                "Cash provided should be greater than to the bill amount")
         }
+    } else {
+        showMessage("Invalid bill amount 😑");
+    }
 });
-
-function calculateChange(amountToBeReturned){
-    for(let i = 0; i <= availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(
-            amountToBeReturned / availableNotes[i]
-            );
-        amountToBeReturned %= availableNotes[i];
-        noOfnotes[i].innerText = numberOfNotes;
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+        if (numberOfNotes > 0) {
+            noOfNotes[i].style['font-size'] = '2rem'
+            noOfNotes[i].style.color = 'red'
+            noOfNotes_td[i].style['font-size'] = '2rem'
+        }
+        noOfNotes[i].setAttribute('data-value',numberOfNotes)
     }
 }
 
-function hideMessage(){
-    message.style.display ="none";
+function hideMessage() {
+    message.style.display = "none";
 }
 
-function showMessage(msg){
-    console.log("here");
-    message.style.display ="block";
+function showMessage(msg) {
+    message.style.display = "block";
     message.innerText = msg;
-
 }
